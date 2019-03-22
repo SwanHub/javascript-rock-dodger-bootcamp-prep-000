10e6603d7d720eb9f7d395eade399f21f8158b45
Jackson Prince <jdprince555@gmail.com>
2019-03-22 22:09:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -128,9 +128,7 @@ function createRock(x) {
  
 function endGame() {
   clearInterval(gameInterval);
-  for (let i = 0; i < ROCKS.length; i++) {
-    ROCKS.pop();
-  }
+  document.querySelectorAll('rock').remove();
  
 }
 
